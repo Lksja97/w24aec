1a9e83ff8317895c6df9f5c9bdcb4730baa9edd1
Ali Khorsandfard <secure73@gmail.com>
2024-10-24 13:11:31 +0200

    constructor and pust map to instance in contact.php

--- app/Message.php
+++ app/Message.php
@@ -2,12 +2,17 @@
 
 class Message {
     public string $body;
-    public string $senderName;
+    public ?string $senderName;
 
-    public string $senderEmail;
-    public string $mobileNumber;
+    public ?string $senderEmail;
+    public ?string $mobileNumber;
 
-
+    public function __construct()
+    {
+        $this->senderName = null;
+        $this->senderEmail = null;
+        $this->mobileNumber = null;
+    }
     public function send():bool{
         return false;
     }
--- contact.php
+++ contact.php
@@ -1,9 +1,35 @@
-<?php  require_once(".\layout\header.php") ?>
+<?php  
+require_once(".\layout\header.php");
+require_once(".\app\Message.php");
+// $visitor_message ist ein Instance von Objekt Message , 
+//$visitor_message ist jetzt in Ram!!
+
+if(isset($_POST["body"]))
+{
+    $visitor_message = new Message();
+    $visitor_message->body = $_POST["body"];
+    $visitor_message->mobileNumber = $_POST["visitor_phone_number"];
+    $visitor_message->senderEmail = $_POST["visitor_email"];
+    $visitor_message->senderName = $_POST["visitor_name"];    
+    unset($_POST);
+    
+    $send_result = $visitor_message->send(); 
+    if($send_result){
+        echo "successful sended message";
+    }
+    else
+    {
+        echo "sorry try later!";
+    }
+
+}
+
+?>
     
 
 <h1>Contact us form</h1>
 
-<form action="">
+<form  method="post">
     <textarea name="body" placeholder="your message" required minlength="10" maxlength="900"></textarea>
     <br/>
     <br/>
@@ -16,7 +42,7 @@
     <input type="string" name="visitor_phone_number"  placeholder="your tel number optional" minlength="8" maxlength="16"/>
     <br/>
     <br/>
-    <button>senden</button>
+    <button type="submit">senden</button>
 </form>
 
 
