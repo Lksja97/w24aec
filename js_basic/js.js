6643af8a72e892c704507cda6364ec7837780019
secure73 <secure73@gmail.com>
2024-10-29 10:32:06 +0100

    jquery

--- js_basic/js.js
+++ js_basic/js.js
@@ -1,45 +1,36 @@
-function my_func(){
-    /*var my_text = "hello js world";
-    var my_second_text ="it is ma world";
-    var my_text = my_text + " it is my world";
-    var my_int = 300;
-    my_int = my_int + 100;
+function my_add(){
+   var first_number = document.getElementById("first_number").value;
+   first_number = parseInt(first_number);
+   var second_number = document.getElementById("second_number").value;
+   second_number = parseInt(second_number);
+   window.alert(first_number+second_number);
+}
+function my_minus(){
+   var first_number = document.getElementById("first_number").value;
+   first_number = parseInt(first_number);
+   var second_number = document.getElementById("second_number").value;
+   second_number = parseInt(second_number);
+   window.alert(first_number - second_number);
+
+}
+function my_multiple(){
+   window.alert("my_multiple");
+
+}
+function my_divide(){
+   window.alert("my_divide");
+
+}
+
+
+$( document ).ready(function() {
+   $("#project_one").hide();
+   $("#btn_show_hide").on("click",function(){
+      $("#project_one").toggle("medium");
+   });
+});
+
+
+
+
  
-    my_int= my_int + my_text;
-
-
-    window.alert(my_int);
-    */
-
-    var my_text = document.getElementById("my_input").value;
-    window.alert(my_text);
-
- 
- }
-
-
- function my_add(){
-    var first_number = document.getElementById("first_number").value;
-    first_number = parseInt(first_number);
-    // parseFloat() function ist auch verhanden
-    var second_number = document.getElementById("second_number").value;
-    second_number = parseInt(second_number);
-    window.alert(first_number+second_number);
- }
- function my_minus(){
-    var first_number = document.getElementById("first_number").value;
-    first_number = parseInt(first_number);
-    // parseFloat() function ist auch verhanden
-    var second_number = document.getElementById("second_number").value;
-    second_number = parseInt(second_number);
-    window.alert(first_number - second_number);
-
- }
- function my_multiple(){
-    window.alert("my_multiple");
-
- }
- function my_divide(){
-    window.alert("my_divide");
-
- }
