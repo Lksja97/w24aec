7b17c41520394dc32ade90c18635384a619dd19c
Ali Khorsandfard <secure73@gmail.com>
2024-10-29 23:37:03 +0100

    .env

--- api/vendor/composer/autoload_real.php
+++ api/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit8891d92c60d63fc66d4f5f941bbf9783
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit8891d92c60d63fc66d4f5f941bbf9783::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
